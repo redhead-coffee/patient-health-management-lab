5489c45de661eee137de1363039d263e8ee475ec
Ezaje Ahamed <ezajeahamed@gmail.com>
2026-06-12 01:50:41 +0530

    Fix address and patient basemodels inside nested-models

--- Pydantic/nested-models_pyd3.py
+++ Pydantic/nested-models_pyd3.py
@@ -9,14 +9,17 @@ class Patient(BaseModel):
     name:str
     gender:str
     age:int
-    address:Address
+    address:Address #nested model--we can use another Pydantic model as a field in our main model. This allows us to create complex data structures and validate them effectively.
 
 
+
+#this should be at first to avoid circular import issues
 address_info={'city':'Bangalore','state':'Karnataka','pin_code':'560001'}
 
 address1=Address(**address_info)
 
-patient_dict={'name':'Nitishh','gender':'Male','age':67,'address':address1} 
+#and then we can use this address1 object to create our patient object. This way we can ensure that the address details are valid before creating the patient object.
+patient_dict={'name':'Nitishh','gender':'Male','age':67,'address':address1}
 
 patient1=Patient(**patient_dict)
 
